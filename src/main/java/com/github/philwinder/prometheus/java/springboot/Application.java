96ea36c3be6e84530b5ad42526f0630178d656a8
Phil Winder <phil@winderresearch.com>
2017-11-09 13:32:20 +0000

    Add comments.

--- src/main/java/com/github/philwinder/prometheus/java/springboot/Application.java
+++ src/main/java/com/github/philwinder/prometheus/java/springboot/Application.java
@@ -7,27 +7,37 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
+// Standard Spring boot annotation
 @SpringBootApplication
+// Add a Prometheus metrics enpoint to the route `/prometheus`. `/metrics` is already taken by Actuator.
 @EnablePrometheusEndpoint
+// Pull all metrics from Actuator and expose them as Prometheus metrics. Must have permission to do this.
 @EnableSpringBootMetricsCollector
+// For route annotations below.
 @RestController
+// Main application class. Keep it in one file for simplicity.
 public class Application {
+    // A Histogram Prometheus Metric
     static final Histogram requestLatency = Histogram.build()
             .name("http_request_duration_seconds")
             .help("HTTP request duration (seconds).")
-            .register();
+            .register();    // Register must be called to add it to the output
 
+    // Standard MVC style route mapping
     @RequestMapping("/")
+    // Note that we could have used the Spring AOP annotation @PrometheusTimeMethod too.
     String root() {
+        // Start the histogram timer
         Histogram.Timer requestTimer = requestLatency.startTimer();
         try {
             return "Hello Spring Boot World!";
         } finally {
+            // Stop the histogram timer.
             requestTimer.observeDuration();
         }
-
     }
 
+    // Standard Spring boot main.
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
